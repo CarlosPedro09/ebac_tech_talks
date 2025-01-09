46e68906b1400c8b443448981a8df8789e10b0de
Carlos <carlos_pedro17@live.com>
2025-01-08 22:44:22 -0300

    contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
-console.log("Olá Mundo");
+AOS.init();
+
+const dataDoEvento = new Date("Feb 17, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const atualizaContador = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;      
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if (distanciaAteOEvento <= 0) {
+       clearInterval(atualizaContador);
+       document.getElementById("contador").innerHTML = "o evento já começou!";
+       return;
+    }
+}, 1000);
+
+
+
+
